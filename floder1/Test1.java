d9e469bb9f9443f79c45f809ac09175a99462e82
IMYIMYIMY <IMYBCN@yahoo.com>
2017-03-20 15:02:34 +0800

    commit_4_what

--- floder1/Test1.java
+++ floder1/Test1.java
@@ -3,7 +3,10 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("1_hehe");
         System.out.println("2_test_diff");
+<<<<<<< Updated upstream
         System.out.println("2_b");
+=======
+>>>>>>> Stashed changes
         System.out.println("4_test_zc_1");
         System.out.println("4_test_zc_3");
     }
